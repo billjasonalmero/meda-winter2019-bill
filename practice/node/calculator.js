1cace962f36e631021b26bb8bbc7151da128ed74
Bill Jason <jasonez_03@yahoo.com>
2019-10-24 11:08:19 -0700

    homwwork1 updated

--- practice/node/calculator.js
+++ practice/node/calculator.js
@@ -27,8 +27,21 @@ if (isNaN(parseInt(terminalArguments[3])  )) {
 }
 
 if(terminalArguments[4] != undefined) {
-  console.log("Unforunately this program can only use the first two numbers. Subscribe for future updates")
+  console.log("Unforunately this program can only use the first two numbers. Subscribe for future updates.")
 }
+
+var number1 = terminalArguments[2];
+var number2 = terminalArguments[3];
+//Safe to use number1 and number2 as they are Number Datatypes.
+add(number1,number2);
+
+
+function add(num1,num2) {
+  var sum = num1 + num2;
+  console.log(sum);
+}
+
+
 
 //More readable an dcan be reused in the futire if we still want the number
 var number1 = parseInt(terminalArguments[2]);
@@ -37,3 +50,36 @@ var sum = number1 + parseInt(terminalArguments[3]);
 
 console.log(sum);
 console.log(true);
+
+
+
+//create divide, multiply, subtract, and leftover function
+
+function divide(num1,num2) {
+  var divide = num1 / num2;
+  console.log(qoutient);
+  
+}
+console.log(divide);
+
+function product(num1,num2) {
+  var product = num1 * num2;
+  console.log(product);
+  
+}
+console.log(product);
+
+function subtract(num1,num2) {
+  var subtract = num1 - num2;
+  console.log(subtract);
+  
+}
+console.log(subtract);
+
+
+function modulo(num1,num2) {
+  var modulo = num1 % num2;
+  console.log(modulo);
+}
+console.log(modulo);
+
