92475b0cfdff47bf8135efe0c84b81f1a61f3fa9
Bill Jason <jasonez_03@yahoo.com>
2019-11-13 12:15:23 -0800

    updated index.js

--- practice/express/request/index.js
+++ practice/express/request/index.js
@@ -23,9 +23,12 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 //Custom Code for Express.js after this line
 
+//variable to make sure the filenameis the same every time. 
+let filename = "commentHistory.json";
+
 //Routes
 //First Argument is the route name, second argument is directory to load when someone requests the route name.
-app.use("/form", express.static("client/"));
+app.use("/", express.static("client/"));
         //route path           //directory path
 //an HTTP Post Handler called submitComment.
 //First argument is HTTP route name, second segment is the callback function. The callback function will run every time someone requests the /submitComment Post handler.
@@ -40,14 +43,13 @@ app.post("/submitComment", (request, response) => {
   //let text = objectFromRequest.message;
   //console.log("We received a request for submitComment: " + text);
 
-  //variable to make sure the filenameis the same every time. 
-  let filename = "commentHistory.json";
+
 
   //If the file exist do...
   if(fs.existsSync(filename)) {
 
     //...read the file and store the contents in the variable comments...
-    let comment = fs.readFileSync(filename, "utf8");
+    let comments = fs.readFileSync(filename, "utf8");
     //..parse the JSON and reuse the same comments variable...
     comments = JSON.parse(comments);
     //...add the new objectFromRequest object to the array inside of the comments object...
@@ -61,7 +63,7 @@ app.post("/submitComment", (request, response) => {
     //If the file doesn't exist...
     //...We rebuild the object (which includes the comment we just recieve) to save as JSON
     let comments = {
-      commentArray: [objectFromRequest]
+      commentsArray: [objectFromRequest]
     };
     //...Convert it actual JSOn string...
     comments = JSON.stringify(comments);
@@ -75,10 +77,27 @@ app.post("/submitComment", (request, response) => {
   response.sendStatus(200);
 });
 
-//request object that has a detail. ex:what the Browser, what is the ip address
-//response  where to send the request 
+//*** request object that has a detail. ex:what the Browser, what is the ip address ***
+//*** response  where to send the request ***
 
-app,post("/loadComments", (request,response) => {
+//A second  HTTP Post Handler called /loadComments
+app.post("/loadComments", (request,response) => {
+
+  //Check if the JSON file exist...
+  if (fs.existsSync(filename) {
+
+    //...if it exist then read it...
+    let comments = fs.readFileSync(filename, "utf8");
+
+    //...and convert to a JavaScript Object
+    comments = JSON.parse(comments);
+
+    //...finally send it to the computer.
+    response.send(comments);
+  }else{
+    //...If it doesn't exist, then send an error 500 to the requester
+    response.sendStatus(500); //displays to the user that there was an issue to the server (500 is an error status)
+  }
 
 })
 
